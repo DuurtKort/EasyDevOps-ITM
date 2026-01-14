6739f823effddc6f3e6f8dbe823709d42bcb109c
DuurtKort <erennkongul78@gmail.com>
2026-01-14 21:41:04 +0100

    .NET EasyDevOps frontend

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("EasyDevOps");
+Console.WriteLine("======================================");
+Console.WriteLine("      ITM - EasyDevOps Frontend      ");
+Console.WriteLine("======================================");
+Console.WriteLine("      ___ _____  __  __              ");
+Console.WriteLine("     |_ _|_   _|/ _|/ _|             ");
+Console.WriteLine("      | |  | | | |_| |_              ");
+Console.WriteLine("     |___| |_|  \\__|\\__|             ");
+Console.WriteLine();
--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c55530f0cbc599a351be5c83c8824ddb1ef6d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342eee61ab901ca321275a8a16cb72f5c6c530e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
