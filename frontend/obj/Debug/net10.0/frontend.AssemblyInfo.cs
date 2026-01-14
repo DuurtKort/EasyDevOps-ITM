e6a70f845107ec8a96231a9241156fe037285e74
DuurtKort <erennkongul78@gmail.com>
2026-01-14 21:21:26 +0100

    Add simple .NET EasyDevOps console frontend

--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfd9b8799734367438164e27f99b3140ac4ec1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c55530f0cbc599a351be5c83c8824ddb1ef6d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
